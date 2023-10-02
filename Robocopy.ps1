385c88161e5431b966faf64f3ef065d4d5d5f9ea
Derelicity17 <derelicity17@outlook.com.au>
2023-10-03 10:10:52 +1100

    Update Robocopy.ps1
    added log explainer.

--- Robocopy.ps1
+++ Robocopy.ps1
@@ -3,4 +3,5 @@ $from = Read-Host -Prompt "`n Enter the From address"
 $to = Read-Host -Prompt "`n Enter the To address"
 
 #MT is multithread, change that if you dont have the number below available or wanna use less.
+#LOG is where you want to dump the log. Below is just an example location.
 robocopy $from $to /E /R:5 /W:5 /MT:8 /LOG:"C:\path\to\log\file.txt"
